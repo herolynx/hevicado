48d467f8c32d4cca7860f111d0ade0f14adde13a
m-wrona <michal.robert.wronski@gmail.com>
2014-06-02 21:44:27 +0200

    [#5] [Auth] Add automatic re-routing when user is not logged in
    UI-Router added to Bower components.
    Current route rules rewritten using UI-Router states.

--- app/js/app.js
+++ app/js/app.js
@@ -8,17 +8,11 @@ angular.module('kunishu',
         'kunishu.filters',
         'kunishu.services',
         'kunishu.directives',
-        'kunishu.controllers'
+        'kunishu.controllers',
+        'kunishu-routes'
     ]
 ).
     value('version', '0.01').
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/userList', {templateUrl: '/app/partials/userList.html', controller: 'EmptyCtrl'});
-        $routeProvider.when('/messages', {templateUrl: '/app/partials/messages.html', controller: 'EmptyCtrl'});
-        $routeProvider.when('/calendar', {templateUrl: '/app/partials/calendar.html', controller: 'EmptyCtrl'});
-        $routeProvider.when('/login', {templateUrl: '/app/modules/auth/partials/login.html', controller: 'LoginCtrl'});
-        $routeProvider.otherwise({redirectTo: '/login'});
-    }]).
     config(function ($httpProvider) {
         $httpProvider.interceptors.push('AuthInterceptor');
     });
--- app/modules/auth/js/auth-app.js
+++ app/modules/auth/js/auth-app.js
@@ -3,6 +3,25 @@
 angular.module('kunishu-auth',
     [
         'kunishu-auth.controllers',
-        'kunishu-auth.services'
+        'kunishu-auth.services',
+        'ui.router'
     ]
-);
+).
+    constant('AUTH_EVENTS', {
+        loginSuccess: 'auth-login-success',
+        loginFailed: 'auth-login-failed',
+        logoutSuccess: 'auth-logout-success',
+        sessionTimeout: 'auth-session-timeout',
+        notAuthenticated: 'auth-not-authenticated',
+        notAuthorized: 'auth-not-authorized'
+    }).
+    constant('USER_ROLES', {
+        admin: 'admin',
+        client: 'client',
+        guest: 'guest'
+    }).
+    constant('ACCESS_LEVELS', {
+        public: '*',
+        user: ['client', 'admin'],
+        admin: ['admin']
+    });
