6077cbdaf5a6295c354ae4523804420d9a49d496
m-wrona <michal.robert.wronski@gmail.com>
2014-06-02 21:53:29 +0200

    [#5] [Auth] Add automatic re-routing when user is not logged in
    Code reformatted to increase readability and documentation added.

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,48 +1,53 @@
 'use strict';
 
-angular.module('kunishu-routes', []).
-    config(function ($stateProvider, $urlRouterProvider, ACCESS_LEVELS) {
-        //
-        // for any unmatched url, redirect to /login
-        $urlRouterProvider.otherwise("/login");
-        //
-        // access level: public
-        $stateProvider.
-            state('login', {
-                url: "/login",
-                templateUrl: "/app/modules/auth/partials/login.html"
-                ,
-                data: {
-                    access: ACCESS_LEVELS.public
-                }
-            })
-        //
-        //
-        // access level: user
-        $stateProvider.
-            state('calendar', {
-                url: "/calendar",
-                templateUrl: "/app/partials/calendar.html",
-                data: {
-                    access: ACCESS_LEVELS.user
-                }
-            }).
-            state('messages', {
-                url: "/messages",
-                templateUrl: "/app/partials/messages.html",
-                data: {
-                    access: ACCESS_LEVELS.user
-                }
-            })
-        //
-        // access level: admin
-        $stateProvider.
-            state('users', {
-                url: "/users",
-                templateUrl: "/app/partials/userList.html",
-                data: {
-                    access: ACCESS_LEVELS.admin
-                }
-            })
-    });
+var routes = angular.module('kunishu-routes', []);
+
+/**
+ * Configure routing rules between application states
+ * @param $stateProvider: application state manager
+ * @param $urlRouterProvider application URL manager
+ * @param ACCESS_LEVELS: available access levels in application
+ */
+routes.config(function ($stateProvider, $urlRouterProvider, ACCESS_LEVELS) {
+    //
+    // for any unmatched url, redirect to /login
+    $urlRouterProvider.otherwise("/login");
+    //
+    // access level: public (access for not logged in users)
+    $stateProvider.
+        state('login', {
+            url: "/login",
+            templateUrl: "/app/modules/auth/partials/login.html",
+            data: {
+                access: ACCESS_LEVELS.public
+            }
+        })
+    //
+    // access level: user (logged in users)
+    $stateProvider.
+        state('calendar', {
+            url: "/calendar",
+            templateUrl: "/app/partials/calendar.html",
+            data: {
+                access: ACCESS_LEVELS.user
+            }
+        }).
+        state('messages', {
+            url: "/messages",
+            templateUrl: "/app/partials/messages.html",
+            data: {
+                access: ACCESS_LEVELS.user
+            }
+        })
+    //
+    // access level: admin
+    $stateProvider.
+        state('users', {
+            url: "/users",
+            templateUrl: "/app/partials/userList.html",
+            data: {
+                access: ACCESS_LEVELS.admin
+            }
+        })
+});
 
