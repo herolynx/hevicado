2e6ee39f72d11f2885bd40476379eb9d4a30b0cc
Michal Wronski <michal.robert.wronski@gmail.com>
2014-09-12 23:16:02 +0200

    [#17] Cosmetic changed to make management of authorized elements more efficient.

--- app/modules/bolt/js/bolt-directives.js
+++ app/modules/bolt/js/bolt-directives.js
@@ -5,14 +5,17 @@ var directives = angular.module('bolt.directives', []);
 /**
  * Definition of authorized element which visibility is controlled based on user's access rights.
  *
- * @param $scope current scope where element is attached to
  * @param parent parent of managed element
  * @param element element which visibility should be controlled
- * @param reqRoles user roles required to shod element
+ * @param reqRoles user roles required to show element
  * @param AuthService service for authorizing user
- * @param AUTH_EVENTS events that will be listened in order to change element's visibility
  */
-function AuthorizedElement($scope, parent, element, reqRoles, AuthService, AUTH_EVENTS) {
+function AuthorizedElement(parent, element, reqRoles, AuthService) {
+
+    this.parent = parent;
+    this.element = element;
+    this.reqRoles = reqRoles;
+    this.AuthService = AuthService;
 
     /**
      * Verify user's current access rights and
@@ -29,27 +32,27 @@ function AuthorizedElement($scope, parent, element, reqRoles, AuthService, AUTH_
     /**
      * Attach managed element to event-bus so its visibility
      * can be changed when proper events occur
+     * @param eventBus event bus where listening will be made
+     * @param AUTH_EVENTS set of events to be listened to in order to change element's visibility
      */
-    AuthorizedElement.prototype.attachToEventBus = function () {
+    AuthorizedElement.prototype.attachToEventBus = function (eventBus, AUTH_EVENTS) {
         var checkAccessRights = this.checkAccessRights;
-        $scope.$on(AUTH_EVENTS.USER_LOGGED_IN, function () {
+        eventBus.$on(AUTH_EVENTS.USER_LOGGED_IN, function () {
             checkAccessRights();
         });
-        $scope.$on(AUTH_EVENTS.USER_LOGGED_OUT, function () {
+        eventBus.$on(AUTH_EVENTS.USER_LOGGED_OUT, function () {
             checkAccessRights();
         });
-        $scope.$on(AUTH_EVENTS.SESSION_TIMEOUT, function () {
+        eventBus.$on(AUTH_EVENTS.SESSION_TIMEOUT, function () {
             checkAccessRights();
         });
     }
 
-    this.checkAccessRights();
-    this.attachToEventBus();
 }
 
 /**
- * Directive manages visibility of chosen element based on requried access rights
- * and user's current access rights.
+ * Directive to wrap chosen element into authorized element that visibility
+ * will be controlled based on user's access rights
  * @param AuthService service for authorizing user
  * @param AUTH_EVENTS events that will be listened in order to change element's visibility
  */
@@ -63,7 +66,9 @@ directives.directive('permission', function (AuthService, AUTH_EVENTS) {
         },
         link: function ($scope, elm, attrs) {
             var reqRoles = $scope.roles.split(',');
-            new AuthorizedElement($scope, elm.parent(), elm, reqRoles, AuthService, AUTH_EVENTS);
+            var authElement = new AuthorizedElement(elm.parent(), elm, reqRoles, AuthService);
+            authElement.attachToEventBus($scope, AUTH_EVENTS);
+            authElement.checkAccessRights();
         }
     }
 
