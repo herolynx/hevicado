6b010ef1cf94f1c86d9d05145b6edbb50b64f9a0
Michal Wronski <michal.robert.wronski@gmail.com>
2014-09-23 18:19:46 +0200

    [#21] Calendar views for one day, week and month attached to application.

--- app/js/routes.js
+++ app/js/routes.js
@@ -12,94 +12,81 @@ routes.config(function ($stateProvider, $urlRouterProvider, ACCESS_LEVELS) {
     //
     // for any unmatched url, redirect to /login
     $urlRouterProvider.otherwise("/login");
-    //
-    // access level: PUBLIC (access for not logged in users)
-    $stateProvider.
-        state('login', {
-            url: "/login",
-            templateUrl: "modules/bolt/partials/login.html",
-            data: {
-                access: ACCESS_LEVELS.PUBLIC
-            }
-        }).
-        state('registration', {
-            url: "/registration",
-            templateUrl: "modules/users/partials/registration.html",
-            data: {
-                access: ACCESS_LEVELS.PUBLIC
-            }
-        }).
-        state('default', {
-            url: "/public",
-            templateUrl: "partials/public.html",
-            data: {
-                access: ACCESS_LEVELS.PUBLIC
-            }
-        }).
-        state('public', {
-            url: "/public",
-            templateUrl: "partials/public.html",
-            data: {
-                access: ACCESS_LEVELS.PUBLIC
-            }
-        });
-    //
-    // access level: USER (logged in users)
-    $stateProvider.
-        state('default-user', {
-            url: "/private",
-            templateUrl: "partials/private.html",
-            data: {
-                access: ACCESS_LEVELS.USER
-            }
-        }).
-        state('user-profile', {
-            url: "/user-profile",
-            templateUrl: "modules/users/partials/user-profile.html",
-            data: {
-                access: ACCESS_LEVELS.USER
-            }
-        }).
-        state('private', {
-            url: "/private",
-            templateUrl: "partials/private.html",
-            data: {
-                access: ACCESS_LEVELS.USER
-            }
-        }).
-        state('dashboard', {
-            url: "/dashboard",
-            templateUrl: "modules/dashboard/partials/dashboard.html",
-            controller: 'DashboardCtrl',
-            data: {
-                access: ACCESS_LEVELS.USER
-            }
-        }).
-        state('calendar', {
-            url: "/calendar",
-            templateUrl: "modules/chronos/partials/calendar.html",
-            controller: 'DashboardCtrl',
-            data: {
-                access: ACCESS_LEVELS.USER
-            }
-        }).
-        state('calendar-events', {
-            url: "/calendar-events",
-            templateUrl: "modules/chronos/partials/calendar-events.html",
-            controller: 'DashboardCtrl',
-            data: {
-                access: ACCESS_LEVELS.USER
-            }
-        });
-    //
-    // access level: ADMIN
-    $stateProvider.
-        state('admin', {
-            url: "/admin",
-            templateUrl: "partials/admin.html",
-            data: {
-                access: ACCESS_LEVELS.ADMIN
-            }
-        });
-});
 
+    /************************* DEFAULT PAGES ***************************************/
+    $stateProvider.
+    state('default', {
+        url: "/main",
+        templateUrl: "partials/main.html",
+        data: {
+            access: ACCESS_LEVELS.PUBLIC
+        }
+    }).
+    state('default-user', {
+        url: "/user-profile",
+        templateUrl: "modules/users/partials/user-profile.html",
+        data: {
+            access: ACCESS_LEVELS.USER
+        }
+    });
+    /************************* LOGIN & REGISTRATION ***************************************/
+    $stateProvider.
+    state('login', {
+        url: "/login",
+        templateUrl: "modules/bolt/partials/login.html",
+        data: {
+            access: ACCESS_LEVELS.PUBLIC
+        }
+    }).
+    state('registration', {
+        url: "/registration",
+        templateUrl: "modules/users/partials/registration.html",
+        data: {
+            access: ACCESS_LEVELS.PUBLIC
+        }
+    });
+    /************************* PUBLIC PAGES ***************************************/
+    $stateProvider.
+    state('main', {
+        url: "/main",
+        templateUrl: "partials/main.html",
+        data: {
+            access: ACCESS_LEVELS.PUBLIC
+        }
+    });
+    /************************* USER MAIN ***************************************/
+    $stateProvider.
+    state('user-profile', {
+        url: "/user-profile",
+        templateUrl: "modules/users/partials/user-profile.html",
+        data: {
+            access: ACCESS_LEVELS.USER
+        }
+    });
+    /************************* CALENDAR ***************************************/
+    $stateProvider.
+    state('calendar-day', {
+        url: "/calendar-day",
+        templateUrl: "modules/chronos/partials/calendar-day.html",
+        controller: 'DashboardCtrl',
+        data: {
+            access: ACCESS_LEVELS.USER
+        }
+    }).
+    state('calendar-week', {
+        url: "/calendar-week",
+        templateUrl: "modules/chronos/partials/calendar-week.html",
+        controller: 'DashboardCtrl',
+        data: {
+            access: ACCESS_LEVELS.USER
+        }
+    }).
+    state('calendar-month', {
+        url: "/calendar-month",
+        templateUrl: "modules/chronos/partials/calendar-month.html",
+        controller: 'DashboardCtrl',
+        data: {
+            access: ACCESS_LEVELS.USER
+        }
+    });
+});
