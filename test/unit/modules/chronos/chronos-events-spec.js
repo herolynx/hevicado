7d080e71531b15d168d5f9aaf05a852c0be8e38f
Michal Wronski <michal.robert.wronski@gmail.com>
2014-10-05 13:21:54 +0200

    [#35] Test cases for event action manager added.

--- test/unit/modules/chronos/chronos-events-spec.js
+++ test/unit/modules/chronos/chronos-events-spec.js
@@ -94,4 +94,74 @@ describe('chronos-events-spec:', function () {
         });
     });
 
+    describe('EventActionManager-spec:', function () {
+        var mockEventStates;
+        var eventActionManager, eventUtils;
+
+        //prepare service for testing
+        beforeEach(angular.mock.module(function ($provide) {
+            //mock dependencies
+            mockEventStates = jasmine.createSpyObj('EVENT_STATE', ['OPEN', 'CLOSED', 'CANCELLED']);
+            mockEventStates.OPEN.key = 0;
+            mockEventStates.CLOSED.key = 1;
+            mockEventStates.CANCELLED.key = 2;
+            $provide.value('EVENT_STATE', mockEventStates);
+
+        }));
+
+        beforeEach(inject(function ($injector) {
+            eventActionManager = $injector.get('EventActionManager');
+            eventUtils = $injector.get('EventUtils');
+        }));
+
+        it('should allow cancel event in OPEN state', function () {
+            //given action manager is initialized
+            expect(eventActionManager).toBeDefined();
+            //and event in open state
+            event = {
+                cancelled: null,
+                start: Date.today().add(1).days()
+            };
+            expect(eventUtils.state(event)).toBe(mockEventStates.OPEN);
+            //when checking whether event can be cancelled
+            var canCancel = eventActionManager.canCancel(event);
+            //then action is allowed
+            expect(canCancel).toBe(true);
+        });
+
+        it('should not allow cancel event in CLOSED state', function () {
+            //given action manager is initialized
+            expect(eventActionManager).toBeDefined();
+            //and event in closed state
+            event = {
+                cancelled: null,
+                start: new Date().set({
+                    day: 1,
+                    month: 1,
+                    year: 2014
+                })
+            };
+            expect(eventUtils.state(event)).toBe(mockEventStates.CLOSED);
+            //when checking whether event can be cancelled
+            var canCancel = eventActionManager.canCancel(event);
+            //then action is denied
+            expect(canCancel).toBe(false);
+        });
+
+        it('should not allow cancel event in CANCELLED state', function () {
+            //given action manager is initialized
+            expect(eventActionManager).toBeDefined();
+            //and event is cancelled already
+            event = {
+                cancelled: Date.today(),
+                start: Date.today()
+            };
+            expect(eventUtils.state(event)).toBe(mockEventStates.CANCELLED);
+            //when checking whether event can be cancelled
+            var canCancel = eventActionManager.canCancel(event);
+            //then action is denied
+            expect(canCancel).toBe(false);
+        });
+    });
+
 });
