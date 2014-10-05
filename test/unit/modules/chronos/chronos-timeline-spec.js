fdb023a8e26e85989e5a30a501582ed8407b682e
Michal Wronski <michal.robert.wronski@gmail.com>
2014-10-05 14:00:50 +0200

    [#35] Test cases for time line controller added.

---
+++ test/unit/modules/chronos/chronos-timeline-spec.js
@@ -0,0 +1,151 @@
+'use strict';
+
+describe('chronos-timeline-spec:', function () {
+
+    //prepare module for testing
+    beforeEach(angular.mock.module('chronos.timeline'));
+
+    describe('TimelineCtrl-spec:', function () {
+        var mockCalendarService, mockUiNotification;
+        var calendarPromise;
+        var ctrlScope;
+
+        //prepare controller for testing
+        beforeEach(inject(function ($controller, _$rootScope_) {
+            //prepare controller for testing
+            ctrlScope = _$rootScope_.$new();
+            //mock dependencies
+            mockCalendarService = jasmine.createSpyObj('mockCalendarService', ['events']);
+            calendarPromise = {
+                success: function (f) {
+                    calendarPromise.success = f;
+                    return calendarPromise;
+                },
+                error: function (f) {
+                    calendarPromise.error = f;
+                    return calendarPromise;
+                }
+            };
+            mockCalendarService.events.andReturn(calendarPromise);
+            mockUiNotification = jasmine.createSpyObj('mockUiNotification', ['text', 'error']);
+            mockUiNotification.text = function (title, msg) {
+                mockUiNotification.title = title;
+                mockUiNotification.msg = msg;
+                return mockUiNotification;
+            };
+            var mockLog = jasmine.createSpyObj('mockLog', ['debug', 'info', 'error']);
+            //inject mocks
+            $controller('TimelineCtrl', {
+                $scope: ctrlScope,
+                $log: mockLog,
+                CalendarService: mockCalendarService,
+                uiNotification: mockUiNotification
+            });
+        }));
+
+        it('should initialize time window', function () {
+            //given ctrl is initialized
+            expect(ctrlScope).toBeDefined();
+            //and start date
+            var startDate = new Date().set({
+                day: 1,
+                month: 0,
+                year: 2014,
+                hour: 0,
+                minute: 0,
+                second: 0
+            });
+            //and days count
+            var daysCount = 7;
+            //when time window is set
+            ctrlScope.initTimeWindow(startDate, daysCount);
+            //then time window is set for the whole week
+            expect(ctrlScope.startDate.toString('yyyy-MM-dd HH:mm:ss')).toBe('2014-01-01 00:00:00');
+            expect(ctrlScope.endDate.toString('yyyy-MM-dd HH:mm:ss')).toBe('2014-01-08 00:00:00');
+        });
+
+        it('should get events from time window', function () {
+            //given ctrl is initialized
+            expect(ctrlScope).toBeDefined();
+            expect(ctrlScope.events).toEqual([]);
+            //and time window
+            var startDate = Date.today();
+            var endDate = Date.today().add(1).days();
+            //and sample events
+            var events = [
+                {
+                    id: "event-1"
+             },
+                {
+                    id: "event-2"
+             }
+            ];
+            //when getting events 
+            ctrlScope.getEvents(startDate, endDate);
+            expect(ctrlScope.loading).toBe(true);
+            //then calendar service is called
+            expect(mockCalendarService.events).toHaveBeenCalledWith(startDate, endDate);
+            calendarPromise.success(events);
+            //and events are loaded properly
+            expect(ctrlScope.loading).toBe(false);
+            expect(ctrlScope.events).toEqual([{
+                id: 'event-2'
+            }, {
+                id: 'event-1'
+            }]);
+        });
+
+        it('should inform user that events cannot be loaded', function () {
+            //given ctrl is initialized
+            expect(ctrlScope).toBeDefined();
+            expect(ctrlScope.events).toEqual([]);
+            //and time window
+            var startDate = Date.today();
+            var endDate = Date.today().add(1).days();
+            //when getting events 
+            ctrlScope.getEvents(startDate, endDate);
+            expect(ctrlScope.loading).toBe(true);
+            //and back-end has responsed with failure
+            expect(mockCalendarService.events).toHaveBeenCalledWith(startDate, endDate);
+            calendarPromise.error('ERROR');
+            //then user is informed properly
+            expect(ctrlScope.loading).toBe(false);
+            expect(mockUiNotification.error).toHaveBeenCalled();
+            expect(mockUiNotification.title).toBe('Error');
+            expect(mockUiNotification.msg).toBe('Couldn\'t load events');
+        });
+
+        it('should load next page of events', function () {
+            //given ctrl is initialized
+            expect(ctrlScope).toBeDefined();
+            //and current time window
+            ctrlScope.startDate = new Date().set({
+                day: 1,
+                month: 0,
+                year: 2014,
+                hour: 0,
+                minute: 0,
+                second: 0
+            });
+            ctrlScope.endDate = new Date().set({
+                day: 1,
+                month: 0,
+                year: 2014,
+                hour: 0,
+                minute: 0,
+                second: 0
+            });
+            ctrlScope.daysCount = 2;
+            //when getting next page of events 
+            ctrlScope.next();
+            //then time window is shifted
+            expect(ctrlScope.startDate.toString('yyyy-MM-dd HH:mm:ss')).toBe('2014-01-01 00:00:00');
+            expect(ctrlScope.endDate.toString('yyyy-MM-dd HH:mm:ss')).toBe('2014-01-03 00:00:00');
+            //and events from new time windows are taken
+            expect(mockCalendarService.events).toHaveBeenCalledWith(ctrlScope.startDate, ctrlScope.endDate);
+            expect(ctrlScope.loading).toBe(true);
+        });
+
+    });
+
+});
