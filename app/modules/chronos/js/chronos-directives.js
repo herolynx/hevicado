68c7602d104e0dc588f6483bd17d6778e53ea8cb
Michal Wronski <michal.robert.wronski@gmail.com>
2014-12-10 19:58:32 +0100

    [#19] Directive for keeping visit form visible all the time added.

--- app/modules/chronos/js/chronos-directives.js
+++ app/modules/chronos/js/chronos-directives.js
@@ -1,3 +1,27 @@
 'use strict';
 
 var directives = angular.module('chronos.directives', []);
+
+/**
+ * Directive for keeping element visible on the screen all the time,
+ * no matter of window scroll position.
+ */
+directives.directive('keepVisible', function ($window) {
+
+    return {
+        restrict: 'A',
+        template: '',
+        link: function ($scope, elm, attrs) {
+            var winElm = angular.element($window);
+            elm.css('position', 'relative');
+            elm.css('top', 0);
+            var shiftDelay = 250;
+            angular.element($window).bind("scroll", function () {
+                var top = winElm.scrollTop();
+                var shiftTop = top - shiftDelay;
+                elm.css('top', shiftTop > 0 ? shiftTop : 0);
+            });
+        }
+    }
+
+});
