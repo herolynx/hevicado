ed6660e1aa657c549d3cfa123bf96457275991b1
Michal Wronski <michal.robert.wronski@gmail.com>
2015-01-01 22:36:04 +0100

    [#20] Degree added to contact info.

--- app/modules/commons/users/js/users-utils.js
+++ app/modules/commons/users/js/users-utils.js
@@ -53,6 +53,7 @@ commonsUtils.value('UserUtils', {
             id: user.id,
             first_name: user.first_name,
             last_name: user.last_name,
+            degree: user.degree,
             email: user.email,
             phone: user.phone,
             role: user.role
