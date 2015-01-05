899abef14f212de58d6cf9093c89f3059df76794
Michal Wronski <michal.robert.wronski@gmail.com>
2015-01-05 09:10:58 +0100

    [#52] Unit tests for date and address filters added.

--- test/unit/modules/commons/users/users-filters-spec.js
+++ test/unit/modules/commons/users/users-filters-spec.js
@@ -59,9 +59,76 @@ describe('commons.users.filters-spec:', function () {
 
     describe('addressInfo-spec:', function () {
 
+        it('should get location info', inject(function ($filter) {
+            //given sample address
+            var address = {
+                street: 'ul. Grabiszynska 8',
+                city: 'Wroclaw',
+                country: 'Polska'
+            };
+            //when getting info about location
+            var info = $filter('addressInfo')(address);
+            //then proper value is returned
+            expect(info).toEqual('ul. Grabiszynska 8, Wroclaw, Polska');
+        }));
+
     });
 
     describe('dateFormat-spec:', function () {
+
+        it('should get default date', inject(function ($filter) {
+            //given sample address
+            var date = Date.today().set(
+                {
+                    year: 2015,
+                    month: 0,
+                    day: 5,
+                    hour: 8,
+                    minute: 30,
+                    second: 0
+                }
+            );
+            //when formatting date
+            var dateString = $filter('dateFormat')(date);
+            //then proper value is returned
+            expect(dateString).toEqual('05-01-2015');
+        }));
+
+        it('should get date and time', inject(function ($filter) {
+            //given sample address
+            var date = Date.today().set(
+                {
+                    year: 2015,
+                    month: 0,
+                    day: 5,
+                    hour: 8,
+                    minute: 30,
+                    second: 0
+                }
+            );
+            //when formatting date
+            var dateString = $filter('dateFormat')(date, 'DD-MM-YYYY HH:mm');
+            //then proper value is returned
+            expect(dateString).toEqual('05-01-2015 08:30');
+        }));
+
+        it('should get name of the week day', inject(function ($filter) {
+            //given sample address
+            var date = Date.today().set(
+                {
+                    year: 2015,
+                    month: 0,
+                    day: 5,
+                    hour: 8,
+                    minute: 30,
+                    second: 0
+                }
+            );
+            //when formatting date
+            var dateString = $filter('dateFormat')(date, 'dddd');
+            //then proper value is returned
+            expect(dateString).not.toBe('');
+        }));
 
     });
 
