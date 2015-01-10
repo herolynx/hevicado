7631d1e384cb2d96f9b7f7390fd53886952ecd2a
Michal Wronski <michal.robert.wronski@gmail.com>
2015-01-10 11:19:16 +0100

    [CM] Google maps api dependency moved to app lib for release purposes.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,10 +24,10 @@ module.exports = function (config) {
             'app/bower_components/angular-ui-sortable/sortable.js',
             'app/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
             'app/bower_components/ngmap/build/scripts/ng-map.js',
-            'http://maps.google.com/maps/api/js',
             /* test dependencies */
             'app/bower_components/angular-mocks/angular-mocks.js',
             /* tested components */
+            'app/lib/**/*.js',
             'app/modules/**/*.js',
             /* tested templates */
             'app/modules/**/*.html',
