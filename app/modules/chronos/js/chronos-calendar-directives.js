ba57332254f6c0f853c3f864af97449528ecf1aa
Michal Wronski <michal.robert.wronski@gmail.com>
2015-01-24 11:29:12 +0100

    [#21] Formatting changed.

--- app/modules/chronos/js/chronos-calendar-directives.js
+++ app/modules/chronos/js/chronos-calendar-directives.js
@@ -35,61 +35,65 @@ chronCalDirectives.directive('calendarTable', function () {
  * @param CALENDAR_EVENTS events used for calendar notifications
  * @param CALENDAR_SETTINGS settings for calendar rendering
  */
-chronCalDirectives.directive('calendarTableEvent', ['$window', 'CALENDAR_EVENTS', 'CALENDAR_SETTINGS', function ($window, CALENDAR_EVENTS, CALENDAR_SETTINGS) {
+chronCalDirectives.directive('calendarTableEvent',
+    ['$window', 'CALENDAR_EVENTS', 'CALENDAR_SETTINGS',
+        function ($window, CALENDAR_EVENTS, CALENDAR_SETTINGS) {
 
-    /**
-     * Set size of the displayed event element
-     * @param elm DOM representation of event
-     * @param event event to be displayed
-     */
-    var setEventSize = function (elm, event) {
-        clear(elm);
-        //set height
-        var quarterRoot = elm.parent();
-        var quarterHeight = quarterRoot.height();
-        var height = event.quarter * quarterHeight;
-        elm.height(height - 1);
-        //set width
-        if (event.overlap !== undefined && event.overlap.value > 1) {
-            //more then one events overlap at the same time
-            var columnWidth = quarterRoot.width() * CALENDAR_SETTINGS.EVENT_WIDTH_PERCENTAGE;
-            var eventWidth = columnWidth / event.overlap.value;
-            elm.width(eventWidth - CALENDAR_SETTINGS.EVENT_WIDTH_MARGIN);
-            var left = eventWidth * event.timeline;
-            elm.css('left', left);
+            /**
+             * Set size of the displayed event element
+             * @param elm DOM representation of event
+             * @param event event to be displayed
+             */
+            var setEventSize = function (elm, event) {
+                clear(elm);
+                //set height
+                var quarterRoot = elm.parent();
+                var quarterHeight = quarterRoot.height();
+                var height = event.quarter * quarterHeight;
+                elm.height(height - 1);
+                //set width
+                if (event.overlap !== undefined && event.overlap.value > 1) {
+                    //more then one events overlap at the same time
+                    var columnWidth = quarterRoot.width() * CALENDAR_SETTINGS.EVENT_WIDTH_PERCENTAGE;
+                    var eventWidth = columnWidth / event.overlap.value;
+                    elm.width(eventWidth - CALENDAR_SETTINGS.EVENT_WIDTH_MARGIN);
+                    var left = eventWidth * event.timeline;
+                    elm.css('left', left);
+                }
+            };
+
+            /**
+             * Clear attributes of displayed elements
+             * @param elm DOM representation of event
+             */
+            var clear = function (elm) {
+                var parent = elm.parent();
+                elm.width(parent.width() * CALENDAR_SETTINGS.EVENT_WIDTH_PERCENTAGE);
+                elm.css('left', 0);
+            };
+
+            return {
+                restrict: 'E',
+                template: '',
+                scope: {
+                    event: '='
+                },
+                link: function ($scope, elm, attrs) {
+                    setEventSize(elm, $scope.event);
+                    //change event size also when window size changes
+                    angular.element($window).bind('resize', function () {
+                        setEventSize(elm, $scope.event);
+                    });
+                    //change when events on calendar changes
+                    $scope.$on(CALENDAR_EVENTS.CALENDAR_RENDER, function () {
+                        setEventSize(elm, $scope.event);
+                    });
+                }
+            };
+
         }
-    };
-
-    /**
-     * Clear attributes of displayed elements
-     * @param elm DOM representation of event
-     */
-    var clear = function (elm) {
-        var parent = elm.parent();
-        elm.width(parent.width() * CALENDAR_SETTINGS.EVENT_WIDTH_PERCENTAGE);
-        elm.css('left', 0);
-    };
-
-    return {
-        restrict: 'E',
-        template: '',
-        scope: {
-            event: '='
-        },
-        link: function ($scope, elm, attrs) {
-            setEventSize(elm, $scope.event);
-            //change event size also when window size changes
-            angular.element($window).bind('resize', function () {
-                setEventSize(elm, $scope.event);
-            });
-            //change when events on calendar changes
-            $scope.$on(CALENDAR_EVENTS.CALENDAR_RENDER, function () {
-                setEventSize(elm, $scope.event);
-            });
-        }
-    };
-
-}]);
+    ]
+);
 
 /**
  * Directive for displaying time-line on the calendar that depicts current time.
