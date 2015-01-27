fbcbdf7abe9713e97b6648e98c3122fbfc82dd03
Michal Wronski <michal.robert.wronski@gmail.com>
2015-01-27 09:27:07 +0100

    [#21] Unit tests for HTTP config added.

--- test/unit/modules/base/services-spec.js
+++ test/unit/modules/base/services-spec.js
@@ -1,8 +1,28 @@
 'use strict';
 
 
-describe('services-spec', function () {
+describe('services-spec:', function () {
 
     beforeEach(angular.mock.module('base.services'));
+
+    describe('HTTP config spec:', function () {
+
+        var mockLocation;
+
+        beforeEach(angular.mock.module(function ($provide) {
+            mockLocation = jasmine.createSpyObj('mockLocation', ['host']);
+            $provide.value('$location', mockLocation);
+        }));
+
+        it('should provide back-end server address', inject(function ($injector) {
+            //given current location
+            mockLocation.host.andReturn('dev.kunishu.com');
+            //when config of HTTP is taken
+            var config = $injector.get('HTTP_CONFIG');
+            //then config contains proper back-end address
+            expect(config.server).toBe('http://dev.kunishu.com:8000');
+        }));
+
+    });
 
 });
