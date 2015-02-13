a856a5f0f68cc00e6fdf8cccd95ee9f07b1d673e
Michal Wronski <michal.robert.wronski@gmail.com>
2015-02-13 14:55:04 +0100

    [#62] Re-captcha tokens cleared.

--- app/modules/users/js/account/users-account.js
+++ app/modules/users/js/account/users-account.js
@@ -14,10 +14,8 @@ angular.module('users.account').
     config(
     ['reCAPTCHAProvider',
         function (reCAPTCHAProvider) {
-            //dev: 6LcY5gETAAAAAAP8BSSwqTg_YAFDAdcHtCglLLYe
-            //prod: 6Ld95gETAAAAANMOdXOqJJJW7tEu2SDTclgWiEoY
 
-            reCAPTCHAProvider.setPublicKey('6LcY5gETAAAAAAP8BSSwqTg_YAFDAdcHtCglLLYe');
+            reCAPTCHAProvider.setPublicKey('6Ld95gETAAAAANMOdXOqJJJW7tEu2SDTclgWiEoY');
 
             reCAPTCHAProvider.setOptions({
                 theme: 'clean'
