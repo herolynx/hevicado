1bd4ec27e383a4a48ee9ed32bf06c992ad181f80
Michal Wronski <michal.robert.wronski@gmail.com>
2015-02-23 07:40:11 +0100

    [CM] Version changed to 1.1.2

--- app/modules/hevicado/js/config/hevicado-config.js
+++ app/modules/hevicado/js/config/hevicado-config.js
@@ -11,7 +11,7 @@ angular.module('hevicado.config', [
  * Application version
  */
 angular.module('hevicado.config').
-    value('version', '1.1.1');
+    value('version', '1.1.2');
 
 /**
  * Service responsible for discovering HTTP back-end configuration
