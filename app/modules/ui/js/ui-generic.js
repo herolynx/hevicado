d5ea7509a7645f3cdc8ae557c86935fa8e3b897a
Michal Wronski <michal.robert.wronski@gmail.com>
2015-04-04 11:20:57 +0200

    [#66] Directive for aligning row look and feel added.

--- app/modules/ui/js/ui-generic.js
+++ app/modules/ui/js/ui-generic.js
@@ -31,3 +31,42 @@ angular.module('ui.generic').
         };
     });
 
+
+/**
+ * Directive aligns height of elements of chosen type in single row
+ */
+angular.module('ui.generic')
+    .directive('alignInRow', function () {
+
+        /**
+         * Find max height of children of given type
+         * @param elm elm containing children
+         * @param type type of element to be checked
+         * @returns {*} positive max height found, negative value otherwise
+         */
+        var findMaxHeight = function (elm, type) {
+            var max = -2000;
+            var children = elm.children();
+            for (var i = 0; i < children.length; i++) {
+                var child = angular.element(children[i]);
+                if (child.hasClass(type) && child.height() > max) {
+                    max = child.height();
+                }
+            }
+            return max;
+        };
+
+        return {
+            restrict: 'A',
+            link: function ($scope, elm, attrs) {
+                var max = findMaxHeight(elm.parent(), 'box');
+                if (max > 0) {
+                    elm.height(max);
+                }
+            }
+        };
+
+    });
+
+
+
