3f09c9107c2d5c3b3b773b9072463602a42eb0e6
Michal Wronski <michal.robert.wronski@gmail.com>
2015-05-19 08:06:14 +0200

    [#57] Refreshing of main menu added.

--- app/modules/hevicado/js/ui/menu.js
+++ app/modules/hevicado/js/ui/menu.js
@@ -29,61 +29,62 @@ angular.module('hevicado.ui').
     });
 
 /**
- * Show sub-options for menu items.
+ * Prepare main menu of the application
  * Note: function must be called directly when whole menu module is loaded.
  */
 angular.module('hevicado.ui').
     run(
-    ['$rootScope',
-        function ($rootScope) {
-            $rootScope.$watch(function () {
-				if($(window).width() < 746) {
-					$("nav li, footer li").hover(
-						function () {
-							$(this).find('ul.subpage').stop(true, true).animate({
-								left: '0px',
-								opacity: '1',
-								top: '0px'
-							}, 800);
-						}, function () {
-							$(this).find('ul.subpage').stop(true, true).animate({
-								left: '0px',
-								opacity: '0',
-								top: '0px'
-							}, 800);
-						}
-					);
-				}
+    ['$rootScope', '$timeout', '$log',
+        function ($rootScope, $timeout, $log) {
+            var prepareMenu = function () {
+                if ($(window).width() > 1025) {
+                    $log.debug('Preparing menu for: desktop');
+                    console.info('Preparing main menu')
+                    $("nav li, footer li").hover(
+                        function () {
+                            $(this).find('ul.subpage').stop(true, true).animate({
+                                left: '0px',
+                                opacity: '1',
+                                top: '0px'
+                            }, 800);
+                        }, function () {
+                            $(this).find('ul.subpage').stop(true, true).animate({
+                                left: '0px',
+                                opacity: '0',
+                                top: '0px'
+                            }, 800);
+                        }
+                    );
+                } else if ($(window).width() > 768 && $(window).width() < 1025) {
+                    $log.debug('Preparing menu for: mobile');
+                    console.info('mobile menu')
+                    $("nav.product li.parrent").each(function () {
+                        $(this).find('a.test').filter(":first").removeAttr('href');
+                    });
+                    $('nav.product li.parrent').on('click.a', function (e) {
+                        e.preventDefault();
+                        $(this).find('ul').slideToggle(200);
+                        $(this).toggleClass('active');
+                        $(this).siblings().find('ul').slideUp(200);
+                        $(this).siblings('.active').toggleClass('active');
+                    });
+                }
+            };
+            //show menu once page is loaded
+            var showMenuPromise = $timeout(function () {
+                prepareMenu();
+                if (showMenuPromise !== undefined) {
+                    $timeout.cancel(showMenuPromise);
+                    showMenuPromise == undefined;
+                }
+            }, 500);
+            //refresh menu
+            $(window).resize(function () {
+                prepareMenu();
+
             });
         }
     ]);
-	
-/**
- * Show / close mobile menu
-*/	
-angular.module('hevicado.ui').
-    run(
-    ['$rootScope',
-        function ($rootScope) {
-            $rootScope.$watch(function () {	
-			if ( $(window).width() > 768 && $(window).width() < 1025) {
-				function explode(){
-					$("nav.product li.parrent").each(function() {
-							$(this).find('a.test').filter(":first").removeAttr('href');
-						});	
-					}
-				setTimeout(explode, 2000);
 
-				$('nav.product li.parrent').on('click.a', function(e) {
-					e.preventDefault();	
-					$(this).find('ul').slideToggle(200);
-					$(this).toggleClass('active');
-					$(this).siblings().find('ul').slideUp(200);
-					$(this).siblings('.active').toggleClass('active');
-				});	
-			}
-		});
-	}
-]);
 
 
