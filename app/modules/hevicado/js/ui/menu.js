e31ef5f90e1efc7840d4cae72301915bb903bf9c
Michal Wronski <michal.robert.wronski@gmail.com>
2015-05-20 12:34:20 +0200

    [#57] Watchers for menu removed to avoid performance issues.

--- app/modules/hevicado/js/ui/menu.js
+++ app/modules/hevicado/js/ui/menu.js
@@ -34,82 +34,84 @@ angular.module('hevicado.ui').
  */
 angular.module('hevicado.ui').
     run(
-    ['$rootScope',
-        function ($rootScope) {
-            $rootScope.$watch(function () {
-				if($(window).width() > 1025) {
-					$("nav li, footer li").hover(
-						function () {
-							$(this).find('ul.subpage').stop(true, true).animate({
-								left: '0px',
-								opacity: '1',
-								top: '0px'
-							}, 800);
-						}, function () {
-							$(this).find('ul.subpage').stop(true, true).animate({
-								left: '0px',
-								opacity: '0',
-								top: '0px'
-							}, 800);
-						}
-					);
-				}
+    ['$rootScope', '$timeout', '$log',
+        function ($rootScope, $timeout, $log) {
+
+            var prepareMenu = function () {
+                if ($(window).width() <= 767) {
+                    $log.debug('Preparing menu for: mobile');
+                    console.info('mobile menu')
+                    $('.open-menu').click(function (e) {
+                        e.stopPropagation();
+                        if ($('.open-menu').hasClass('small-opened')) {
+                            $('.product').animate({right: 0}, "slow");
+                            $('#page').animate({right: 140}, "slow").addClass('disable');
+                            $('header').animate({right: 140}, "slow");
+                            $(this).removeClass('small-opened');
+                            $(this).addClass('close-menu');
+                        }
+                    });
+                    $('html, body').click(function () {
+                        if ($('.open-menu').hasClass('close-menu')) {
+                            $('.product').animate({right: -140}, "slow");
+                            $('#page').animate({right: 0}, "slow").removeClass('disable');
+                            $('header').animate({right: 0}, "slow");
+                            $('.open-menu').addClass('small-opened');
+                            $('.open-menu').removeClass('close-menu');
+                        }
+                    });
+                } else if ($(window).width() > 768 && $(window).width() < 1024) {
+                    $log.debug('Preparing menu for: tablet');
+                    console.info('tablet menu')
+                    $timeout(function () {
+                        $("nav.product li.parrent").each(function () {
+                            $(this).find('a.test').filter(":first").removeAttr('href');
+                        });
+                    }, 2000);
+
+                    $('nav.product li.parrent').on('click.a', function (e) {
+                        e.preventDefault();
+                        $(this).find('ul').slideToggle(200);
+                        $(this).toggleClass('active');
+                        $(this).siblings().find('ul').slideUp(200);
+                        $(this).siblings('.active').toggleClass('active');
+                    });
+                } else if ($(window).width() > 1025) {
+                    $log.debug('Preparing menu for: desktop');
+                    console.info('desktop menu')
+                    $("nav li, footer li").hover(
+                        function () {
+                            $(this).find('ul.subpage').stop(true, true).animate({
+                                left: '0px',
+                                opacity: '1',
+                                top: '0px'
+                            }, 800);
+                        }, function () {
+                            $(this).find('ul.subpage').stop(true, true).animate({
+                                left: '0px',
+                                opacity: '0',
+                                top: '0px'
+                            }, 800);
+                        }
+                    );
+                }
+            };
+
+            //show menu once page is loaded
+            var showMenuPromise = $timeout(function () {
+                prepareMenu();
+                if (showMenuPromise !== undefined) {
+                    $timeout.cancel(showMenuPromise);
+                    showMenuPromise == undefined;
+                }
+            }, 500);
+
+            //refresh menu
+            $(window).resize(function () {
+                prepareMenu();
             });
         }
     ]);
-	
-/**
- * Show / close mobile menu
-*/	
-angular.module('hevicado.ui').
-    run(
-    ['$rootScope',
-        function ($rootScope) {
-            $rootScope.$watch(function () {	
-			if ( $(window).width() > 768 && $(window).width() < 1024) {
-				function explode(){
-					$("nav.product li.parrent").each(function() {
-							$(this).find('a.test').filter(":first").removeAttr('href');
-						});	
-					}
-				setTimeout(explode, 2000);
-
-				$('nav.product li.parrent').on('click.a', function(e) {
-					e.preventDefault();	
-					$(this).find('ul').slideToggle(200);
-					$(this).toggleClass('active');
-					$(this).siblings().find('ul').slideUp(200);
-					$(this).siblings('.active').toggleClass('active');
-				});
-			}
-			
-			if( $(window).width() <= 767 ){
-					$('.open-menu').click(function (e) {
-					e.stopPropagation();
-						if($('.open-menu').hasClass('small-opened')) {
-							$('.product').animate({right: 0}, "slow");
-							$('#page').animate({right: 140}, "slow").addClass('disable');
-							$('header').animate({right: 140}, "slow");
-							$(this).removeClass('small-opened');
-							$(this).addClass('close-menu');
-						}
-					});
-				}
-				
-				$('html, body').click(function () {
-					if($('.open-menu').hasClass('close-menu')) {
-						$('.product').animate({right: -140}, "slow");
-						$('#page').animate({right: 0}, "slow").removeClass('disable');
-						$('header').animate({right: 0}, "slow");
-						$('.open-menu').addClass('small-opened');
-						$('.open-menu').removeClass('close-menu');
-					}
-				});
-			
-			
-		});
-	}
-]);
 
 
 
