c2f057ff212581f94444625bd2d0f59bea085300
Michal Wronski <michal.robert.wronski@gmail.com>
2015-06-16 22:14:32 +0200

    [#118] News for 1.3 release added.

--- fe/app/modules/hevicado/js/config/hevicado-config.js
+++ fe/app/modules/hevicado/js/config/hevicado-config.js
@@ -11,7 +11,7 @@ angular.module('hevicado.config', [
  * Application version
  */
 angular.module('hevicado.config').
-    value('version', '1.3.0-alpha');
+    value('version', '1.3.0');
 
 /**
  * Generic configuration
