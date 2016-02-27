aae30830a1c86a139c4cd8beff8fc1f8ee748ebd
Michal Wronski <michal.robert.wronski@gmail.com>
2016-02-27 23:16:38 +0100

    [#164] Context of page menu can be changed using values property.

--- fe-react/app/scripts/calendar/components/pages/calendar-page.jsx
+++ fe-react/app/scripts/calendar/components/pages/calendar-page.jsx
@@ -5,10 +5,30 @@ import CalendarWeekly from '../views/calendar-weekly';
 
 export default class CalendarPage extends React.Component {
 
-  render() {
-    return (
+  pageMenuItems() {
+    return {
+      title: 'Kalendarz',
+      items: [
+        {
+          id: 'data-tab-1',
+          name: 'Dzienny',
+          uri: '#',
+          class: 'fa fa-calendar'
+        }, {
+          id: 'data-tab-2',
+          name: 'Tygodniowy',
+          uri: '#/calendar/weekly',
+          class: 'fa fa-calendar',
+          selected: true
+        }
+      ]
+    };
+  }
+
+  render () {
+    return(
       <section id="main-content">
-        <PageMenu/>
+        <PageMenu values={this.pageMenuItems()}/>
         <CalendarWeekly/>
       </section>
     );
--- fe-react/app/scripts/core/frame/page-menu.jsx
+++ fe-react/app/scripts/core/frame/page-menu.jsx
@@ -4,49 +4,25 @@ export default class PageMenu extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      user: props.user
-    };
   }
-
-  static defaultProps = {
-    mainContent: {
-      name: 'Gabinet',
-      personalData: 'Twoje dane',
-      payments: 'Płatności'
-    }
-  };
 
   render () {
     return(
       <div id="title">
         <div className="content">
-          <h2>{this.props.mainContent.name}</h2>
+          <h2>{this.props.values.title}</h2>
           <nav>
             <ul>
-              <li className="tab-link current" data-tab="tab-1">
-                <a href="#">
-                  <i className="fa fa-user"></i>
-                  <span>{this.props.mainContent.personalData}</span>
-                </a>
-              </li>
-              <li className="tab-link" data-tab="tab-2">
-                <a href="#">
-                  <i className="fa fa-credit-card"></i>
-                  <span>{this.props.mainContent.payments}</span>
-                </a>
-              </li>
+              {this.props.values.items.map(item => {
+                return <li className={item.selected ? 'tab-link current' : 'tab-link'} key={item.id} data-tab={item.id}>
+                  <a href={item.uri}>
+                    <i className={item.class}></i>
+                    <span>{item.name}</span>
+                  </a>
+                </li>;
+              })}
             </ul>
           </nav>
-          <div className="action">
-            <a href="#">
-              <i className="fa fa-bell-o"></i>
-              <span>20</span>
-            </a>
-            <a href="#">
-              <i className="fa fa-power-off"></i>
-            </a>
-          </div>
         </div>
       </div>
     );
